b06652e83d0baf66968cfa6b626f20e6b6a0dca4
danaivach <danai.vacht@gmail.com>
2022-11-02 18:35:45 +0100

    feat(signifiers) add agents with bodies and abilities

---
+++ src/main/java/ch/unisg/ics/interactions/hmas/interaction/signifiers/CapableAgent.java
@@ -0,0 +1,51 @@
+package ch.unisg.ics.interactions.hmas.interaction.signifiers;
+
+import ch.unisg.ics.interactions.hmas.core.hostables.Agent;
+import com.google.common.collect.ImmutableSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CapableAgent extends Agent {
+  private final Set<Ability> abilities;
+
+  protected CapableAgent(AbstractBuilder builder) {
+    super(builder);
+    this.abilities = ImmutableSet.copyOf(builder.abilities);
+  }
+
+  public Set<Ability> getAbilities() {
+    return this.abilities;
+  }
+
+  public static class Builder extends AbstractBuilder<Builder, CapableAgent> {
+
+    public CapableAgent build() {
+      return new CapableAgent(this);
+    }
+  }
+
+  public abstract static class AbstractBuilder<S extends AbstractBuilder, T extends CapableAgent>
+          extends Agent.AbstractBuilder<S, T> {
+
+    private final Set<Ability> abilities;
+
+    public AbstractBuilder() {
+      this.abilities = new HashSet<>();
+    }
+
+    public S addAbility(Ability ability) {
+      this.abilities.add(ability);
+      return (S) this;
+    }
+
+    public S addAbilities(Set<Ability> abilities) {
+      this.abilities.addAll(abilities);
+      return (S) this;
+    }
+
+    public abstract T build();
+  }
+}
+
+
---
+++ src/main/java/ch/unisg/ics/interactions/hmas/interaction/signifiers/SituatedAgent.java
@@ -0,0 +1,49 @@
+package ch.unisg.ics.interactions.hmas.interaction.signifiers;
+
+import com.google.common.collect.ImmutableSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SituatedAgent extends CapableAgent {
+  private final Set<AgentBody> bodies;
+
+  protected SituatedAgent(AbstractBuilder builder) {
+    super(builder);
+    this.bodies = ImmutableSet.copyOf(builder.bodies);
+  }
+
+  public Set<AgentBody> getBodies() {
+    return this.bodies;
+  }
+
+  public static class Builder extends AbstractBuilder<Builder, SituatedAgent> {
+
+    public SituatedAgent build() {
+      return new SituatedAgent(this);
+    }
+  }
+
+  public abstract static class AbstractBuilder<S extends AbstractBuilder, T extends SituatedAgent>
+          extends CapableAgent.AbstractBuilder<S, T> {
+
+    private final Set<AgentBody> bodies;
+
+    public AbstractBuilder() {
+      this.bodies = new HashSet<>();
+    }
+
+    public S addBody(AgentBody body) {
+      this.bodies.add(body);
+      return (S) this;
+    }
+
+    public S addBodies(Set<AgentBody> bodies) {
+      this.bodies.addAll(bodies);
+      return (S) this;
+    }
+
+    public abstract T build();
+  }
+}
+
